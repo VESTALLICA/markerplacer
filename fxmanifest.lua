c74b98ad3425da66ca6f16f690b1a3b3727b1da5
VESTA <147888622+VESTALLICA@users.noreply.github.com>
2023-10-19 02:34:05 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,10 +2,10 @@ fx_version 'cerulean'
 game 'gta5'
 
 
-author 'MUDCRAB'
+author 'VESTA'
 description 'Advanced Blip Creator'
 discord 'https://discord.gg/ktzjBM2MkZ'
-scriptname 'mc-blipsandzones'
+scriptname 'markerplacer'
 
 
 shared_script 'config.lua'
